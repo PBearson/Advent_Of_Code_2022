d33f6c448a963ad70d4061b01fe4b7e9b214644b
Bryan Pearson <peabryan95@gmail.com>
2022-12-03 08:22:46 -0500

    Add day 3 part 1

---
+++ day_3/priorities.py
@@ -0,0 +1,31 @@
+# Find the common item between the compartments in each rucksack, and calculate its priority. Do this 
+# for all rucksacks and sum all the priorities.
+
+import string
+
+with open("day_3/input.txt", "r") as f:
+    input = f.readlines()
+input = [i.replace("\n", "") for i in input]
+
+items = string.ascii_letters
+
+total_priority = 0
+
+# Iterate through all rucksacks
+for rucksack in input:
+    compartment_len = int(len(rucksack) / 2)
+    compartment_a = rucksack[:compartment_len]
+    compartment_b = rucksack[compartment_len:]
+    
+    # Find the common item using a simple search
+    common_items = ""
+    for item in compartment_a:
+        if item in compartment_b:
+            common_item = item
+            break
+
+    # Get the priority of the common item
+    priority = items.find(common_item) + 1
+    total_priority += priority
+
+print("Sum of priorities: %d" % total_priority)
