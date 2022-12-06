18df60a60f676640ec64c450ddc8048320168435
Bryan Pearson <peabryan95@gmail.com>
2022-12-06 09:02:03 -0500

    Add day 6 part 2

---
+++ day_6/detect_message.py
@@ -0,0 +1,17 @@
+# Find how many characters need to be processed before the first message is detected.
+
+with open("day_6/input.txt", "r") as f:
+    input = f.readlines()
+input = [i.replace("\n", "") for i in input]
+
+characters = input[0]
+processed = 0
+
+# Get each contiguous sequence of 4 characters and check if they are unique
+for i in range(len(characters) - 13):
+    marker = characters[i:i+14]
+    if len(set(marker)) == 14:
+        processed = i + 14
+        break
+
+print("Processed %d characters" % processed)
